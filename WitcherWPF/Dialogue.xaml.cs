b17ed7c290f25f14b23d93cd92190e217a58cae9
WhiteWolf <viskot007@gmail.com>
2018-12-04 22:56:48 +0100

    Upgraded Dialogue system

--- WitcherWPF/Dialogue.xaml.cs
+++ WitcherWPF/Dialogue.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WitcherWPF
 {
@@ -20,16 +21,16 @@ namespace WitcherWPF
     /// </summary>
     public partial class Dialogue : Page
     {
+        static bool FoltestHelp = true;
         bool Begin = true;
         bool First = false;
         bool Second = false;
         bool Third = false;
         bool Fourth = false;
-
-        bool FoltestHelp = false;
         string Char = "";
         string DialogPart = "";
         private Frame parentFrame;
+        DispatcherTimer timer = new DispatcherTimer();
         public Dialogue()
         {
             InitializeComponent();
@@ -46,31 +47,110 @@ namespace WitcherWPF
             this.parentFrame = parentFrame;
         }
         public async void FoltestDialogue() {
+            Option1.Visibility = Visibility.Hidden;
+            Option2.Visibility = Visibility.Hidden;
+            Option3.Visibility = Visibility.Hidden;
+            Option4.Visibility = Visibility.Hidden;
+            Option5.Visibility = Visibility.Hidden;
             if (Begin == true) {
                 PersonName.Content = "Foltest";
                 PersonText.Content = "Welcome Geralt";
                 await Task.Delay(2000);
                 DialogueOptions.Visibility = Visibility.Visible;
-                Option1.Visibility = Visibility.Visible;
-                Option1.Content = "Do you need any help?";
+                if (FoltestHelp == true) {
+                    Option1.Visibility = Visibility.Visible;
+                    Option1.Content = "Do you need any help?";
+                }
                 Option2.Visibility = Visibility.Visible;
                 Option2.Content = "What is the situation in the city?";
+                Option5.Visibility = Visibility.Visible;
+                Option5.Content = "GoodBye";
                 First = false;
             }
-            if (First == true) {
+            if (DialogPart == "Do you need any help?") {
                 PersonName.Content = "Foltest";
-                PersonText.Content = "Geralt, I need your help";
+                PersonText.Content = "Yes Geralt I have a problem that you can take care off";
                 await Task.Delay(5000);
                 PersonName.Content = "Geralt";
                 PersonText.Content = "What do you need sire?";
                 await Task.Delay(5000);
                 PersonName.Content = "Foltest";
                 PersonText.Content = "There is a monster in the city. Will you kill it?";
+                await Task.Delay(5000);
                 DialogueOptions.Visibility = Visibility.Visible;
                 Option1.Content = "Yes";
-                Option2.Content = "No";
+                Option2.Content = "No, I cannot help you now";
                 Option1.Visibility = Visibility.Visible;
                 Option2.Visibility = Visibility.Visible;
+            }
+            if (DialogPart == "Yes") {
+                PersonName.Content = "Geralt";
+                PersonText.Content = "Yes, I can do that but i need to know more than that";
+                await Task.Delay(5000);
+                PersonName.Content = "Foltest";
+                PersonText.Content = "Well... I do not know much about the monster but a local smith saw it. He can tell you more about it";
+                await Task.Delay(5000);
+                PersonName.Content = "Geralt";
+                PersonText.Content = "Fine, i will go and ask him";
+                Quest MonsterHunt = new Quest();
+                MonsterHunt.NewQuest("Main","Silent Killer","Foltest asked Geralt to hunt down a monster that was murdering the last citizens that stayed in Wyzima. Geralt Accepted and was told to go to a local smith and ask him about the details","Ask the Wyzima's smith about the monster", 1);
+                QueName.Content = "New Quest: Silent Killer";
+                QueGoal.Content = "Ask the Wyzima's smith about the monster";
+                MonsterHunt.QuestShow(QuestPop); 
+                QuestTimer();
+                await Task.Delay(5000);
+                if (FoltestHelp == true) {
+                    Option1.Visibility = Visibility.Visible;
+                    Option1.Content = "Do you need any help?";
+                }
+                Option2.Visibility = Visibility.Visible;
+                Option2.Content = "What is the situation in the city?";
+                Option5.Visibility = Visibility.Visible;
+                Option5.Content = "GoodBye";
+            }
+            if (DialogPart == "No, I cannot help you now") {
+                PersonName.Content = "Geralt";
+                PersonText.Content = DialogPart;
+                await Task.Delay(5000);
+                PersonName.Content = "Foltest";
+                PersonText.Content = "Alright Geralt I cannot force you into anything after you saved my life, but please try to kill it. This city has suffered enough";
+                await Task.Delay(5000);
+                Option2.Visibility = Visibility.Visible;
+                Option2.Content = "What is the situation in the city?";
+                Option5.Visibility = Visibility.Visible;
+                Option5.Content = "GoodBye";
+            }
+            if (DialogPart == "What is the situation in the city?") {
+                PersonName.Content = "Geralt";
+                PersonText.Content = DialogPart;
+                await Task.Delay(5000);
+                PersonName.Content = "Foltest";
+                PersonText.Content = "People are still scared after what happened here";
+                await Task.Delay(5000);
+                PersonName.Content = "Foltest";
+                PersonText.Content = "And Wyzima is completely destoryed. Almost everything has burned down";
+                await Task.Delay(5000);
+                PersonName.Content = "Foltest";
+                PersonText.Content = "Yes.. the situation is very bad, but i will do my best to bring Wyzima back to life";
+                QuestPop.Visibility = Visibility.Hidden;
+                if (FoltestHelp == true) {
+                    Option1.Visibility = Visibility.Visible;
+                    Option1.Content = "Do you need any help?";
+                }
+                Option2.Visibility = Visibility.Visible;
+                Option2.Content = "What is the situation in the city?";
+                Option5.Visibility = Visibility.Visible;
+                Option5.Content = "GoodBye";
+                
+            }
+            if (DialogPart == "GoodBye") {
+                PersonName.Content = "Geralt";
+                PersonText.Content = DialogPart;
+                await Task.Delay(5000);
+                PersonName.Content = "Foltest";
+                PersonText.Content = "Farewell Geralt";
+                await Task.Delay(5000);
+                parentFrame.Navigate(new Map(parentFrame));
             }
         }
 
@@ -79,29 +159,72 @@ namespace WitcherWPF
             if (sender is Button button1) {
                 if (button1.Name == "Option1") {
                     if (Char == "Foltest") {
-                        First = true;
+                        if (Option1.Content.ToString() == "Do you need any help?") {
+                            First = true;
+                            Begin = false;
+                            DialogPart = "Do you need any help?";
+                            FoltestDialogue();
+
+                            Option1.Visibility = Visibility.Hidden;
+                            Option2.Visibility = Visibility.Hidden;
+                            Option3.Visibility = Visibility.Hidden;
+                            Option4.Visibility = Visibility.Hidden;
+                            Option5.Visibility = Visibility.Hidden;
+                        }
+                        if (Option1.Content.ToString() == "Yes") {
+                            DialogPart = "Yes";
+                            FoltestHelp = false;
+                        }
                         Begin = false;
                         FoltestDialogue();
-                        Option1.Visibility = Visibility.Hidden;
-                        Option2.Visibility = Visibility.Hidden;
-                        Option3.Visibility = Visibility.Hidden;
-                        Option4.Visibility = Visibility.Hidden;
-                        Option5.Visibility = Visibility.Hidden;
-                        if (Option1.Content.ToString() == "Yes") {
-
-                        }
                     }
+
                 }
                 else if (button1.Name == "Option2") {
+                    if (Char == "Foltest") {
+                        if (Option2.Content.ToString() == "No, I cannot help you now") {
+                            DialogPart = "No, I cannot help you now";
+                        }
+                        if (Option2.Content.ToString() == "What is the situation in the city?") {
+                            DialogPart = "What is the situation in the city?";
 
-                }else if(button1.Name == "Option3") {
+                        }
+                        Begin = false;
+                        FoltestDialogue();
+                    }
+                    
+                }
+                else if(button1.Name == "Option3") {
 
                 }else if(button1.Name == "Option4") {
 
                 }else if(button1.Name == "Option5") {
+                    if (Char == "Foltest") {
+                        if (Option5.Content.ToString() == "GoodBye") {
+                            DialogPart = "GoodBye";
+                        }
+                        Begin = false;
+                        FoltestDialogue();
+                    }
 
                 }
+                Option1.Visibility = Visibility.Hidden;
+                Option2.Visibility = Visibility.Hidden;
+                Option3.Visibility = Visibility.Hidden;
+                Option4.Visibility = Visibility.Hidden;
+                Option5.Visibility = Visibility.Hidden;
+                Begin = false;
             }
+        }
+        public void QuestTimer() {
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 10000);
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start();
+        }
+        void timer_Tick(object sender, EventArgs e) {
+            Quest hider = new Quest();
+            hider.QuestHide(QuestPop);
+            timer.Stop();
         }
     }
 }
---
+++ WitcherWPF/Quest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Animation;
+using System.Windows.Threading;
+
+namespace WitcherWPF {
+    class Quest {
+        public string QuestType { get; set; }
+        public string QuestName { get; set; }
+        public string QuestDescription { get; set; }
+        public string QuestGoal { get; set; }
+        public int QuestLevel { get; set; }
+
+        public void NewQuest(string QuestType, string QuestName, string QuestDescription, string QuestGoal, int QuestLevel) {
+            this.QuestType = QuestType;
+            this.QuestName = QuestName;
+            this.QuestDescription = QuestDescription;
+            this.QuestGoal = QuestGoal;
+            this.QuestLevel = QuestLevel;
+
+        }
+        public void QuestShow(System.Windows.Controls.StackPanel Tran) {
+            Tran.Visibility = Visibility.Visible;
+            var animation = new DoubleAnimation {
+                To = 1,
+                BeginTime = TimeSpan.FromSeconds(1),
+                Duration = TimeSpan.FromSeconds(2),
+                FillBehavior = FillBehavior.Stop
+            };
+
+            animation.Completed += (s, a) => Tran.Visibility = Visibility.Visible;
+            animation.Completed += (s, a) => Tran.Opacity = 1;
+            Tran.BeginAnimation(UIElement.OpacityProperty, animation);
+        }
+        public void QuestHide(System.Windows.Controls.StackPanel Tran) {
+            var animation = new DoubleAnimation {
+                To = 0,
+                BeginTime = TimeSpan.FromSeconds(1),
+                Duration = TimeSpan.FromSeconds(2),
+                FillBehavior = FillBehavior.Stop
+            };
+
+            animation.Completed += (s, a) => Tran.Visibility = Visibility.Hidden;
+
+            Tran.BeginAnimation(UIElement.OpacityProperty, animation);
+        }
+        
+    }
+}
