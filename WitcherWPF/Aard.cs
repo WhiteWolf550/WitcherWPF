86469dbd4001c17f3b139ba1f38a943cc17c13d0
Rosta Viskot <viskotr@hotmail.com>
2018-12-06 12:46:16 +0100

    new

--- WitcherWPF/Aard.cs
+++ WitcherWPF/Aard.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace WitcherWPF {
     class Aard : Sign {
+        public int StaggerChance { get; set; }
         public int StunChance { get; set; }
     }
 }
